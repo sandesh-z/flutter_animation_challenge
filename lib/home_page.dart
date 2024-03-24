e87ea374ec14d22d610d49d358b4435380feb061
sandesh <gsandace@gmail.com>
2024-03-24 21:00:57 +0545

    example uses of AnimatedController

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool animate = false;
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Flutter animation challenge"),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter animation challenge"),
       ),
+      floatingActionButton: ElevatedButton(
+          onPressed: () {
+            setState(() {
+              animate = !animate;
+            });
+          },
+          child: const Text("Animate")),
+      body: Center(
+          child: AnimatedContainer(
+              color: Colors.deepPurple,
+              height: animate ? 400 : 200,
+              width: 200,
+              duration: const Duration(milliseconds: 500))),
     );
   }
 }
