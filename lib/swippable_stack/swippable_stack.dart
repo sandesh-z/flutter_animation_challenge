52d76243b62a930ea812b524b0b8b8d8776ccfcd
sandesh <gsandace@gmail.com>
2024-03-26 23:17:00 +0545

    custom widget added to position cards from dynamic list

--- lib/swippable_stack/swippable_stack.dart
+++ lib/swippable_stack/swippable_stack.dart
@@ -12,6 +12,12 @@ class SwippableStack extends StatefulWidget {
 class _SwippableStackState extends State<SwippableStack>
     with SingleTickerProviderStateMixin {
   late AnimationController animationController;
+  List<String> imageList = [
+    Assets.dunePoster,
+    Assets.inceptionPoster,
+    Assets.interstellarPoster,
+    Assets.madMaxPoster
+  ];
   @override
   void initState() {
     super.initState();
@@ -35,49 +41,56 @@ class _SwippableStackState extends State<SwippableStack>
       body: Center(
         child: Stack(
           children: [
-            Positioned(
-                child: Transform.translate(
-              offset: const Offset(35, 0),
-              child: AnimatedBuilder(
-                animation: animationController,
-                child: CardWidget(
-                    image: Image.asset(
-                  Assets.madMaxPoster,
-                  fit: BoxFit.cover,
-                )),
-                builder: (context, child) => Transform.rotate(
-                  angle: animationController.value * -6.03,
-                  child: child,
-                ),
-              ),
-            )),
-            Positioned(
-                child: Transform.translate(
-              offset: const Offset(-35, 0),
-              child: AnimatedBuilder(
-                animation: animationController,
-                child: CardWidget(
-                    image: Image.asset(
-                  Assets.interstellarPoster,
-                  fit: BoxFit.cover,
-                )),
-                builder: (context, child) => Transform.rotate(
-                  angle: animationController.value * 6.03,
-                  child: child,
-                ),
-              ),
-            )),
-            Positioned(
-              child: CardWidget(
-                image: Image.asset(
-                  Assets.dunePoster,
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
+            ..._buildPositionedWidgets(imageList),
           ],
         ),
       ),
     );
+  }
+
+  List<Positioned> _buildPositionedWidgets(List<String> imageList) {
+    List<Positioned> positionedWidgets = [];
+
+    // Add widgets for indices other than 0
+    for (int index = 1; index < imageList.length; index++) {
+      final isEven = index.isEven;
+      final offsetX = isEven ? 35.0 : -35.0;
+      final angle = isEven ? -6.03 : 6.03;
+
+      positionedWidgets.add(
+        Positioned(
+          child: Transform.translate(
+            offset: Offset(offsetX, 0),
+            child: AnimatedBuilder(
+              animation: animationController,
+              child: CardWidget(
+                image: Image.asset(
+                  imageList[index],
+                  fit: BoxFit.cover,
+                ),
+              ),
+              builder: (context, child) => Transform.rotate(
+                angle: animationController.value * angle,
+                child: child,
+              ),
+            ),
+          ),
+        ),
+      );
+    }
+
+    // Add widget for index 0 (on top of the stack)
+    positionedWidgets.add(
+      Positioned(
+        child: CardWidget(
+          image: Image.asset(
+            imageList[0],
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+
+    return positionedWidgets;
   }
 }
